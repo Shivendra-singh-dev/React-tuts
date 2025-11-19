f40c78de86c8361476d41b09793633915a010bf6
Shiva <yesmyhomepoint@gmail.com>
2025-11-19 14:50:31 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import './App.css';
+import Navbar from "./Navbar";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="App-link">          Welcome to React Tutorials!
-        </p>
-      </header>
-    </div>
+   <>
+    <Navbar/>
+   </>
   );
 }
 
---
+++ src/components/Item5.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Item1() {
+  return (
+    <>
+     <h1>Item 1</h1> 
+    </>
+  );
+}
